69fd82e8fda3f9a8931c83f73744ed87a9482c17
adammendak <adam.mendak@gmail.com>
2019-12-16 17:49:14 +0100

    more

---
+++ src/com/adammendak/wet/concurrency/odsetki2/SynchBankTest3.java
@@ -0,0 +1,42 @@
+package com.adammendak.wet.concurrency.odsetki2;
+
+/**
+ * This program shows how multiple threads can safely access a data structure,
+ * using synchronized methods.
+ * @version 1.32 2018-04-10
+ * @author Cay Horstmann
+ */
+public class SynchBankTest3
+{
+   public static final int NACCOUNTS = 100;
+   public static final double INITIAL_BALANCE = 1000;
+   public static final double MAX_AMOUNT = 1000;
+   public static final int DELAY = 10;
+
+   public static void main(String[] args)
+   {
+      Bank bank = new Bank(NACCOUNTS, INITIAL_BALANCE);
+      for (int i = 0; i < NACCOUNTS; i++)
+      {
+         int fromAccount = i;
+         Runnable r = () -> {
+            try
+            {
+               while (true)
+               {
+                  int toAccount = (int) (bank.size() * Math.random());
+                  double amount = MAX_AMOUNT * Math.random();
+                  bank.transfer(fromAccount, toAccount, amount);
+                  bank.addInterest(fromAccount);
+                  Thread.sleep((int) (DELAY * Math.random()));
+               }
+            }
+            catch (InterruptedException e)
+            {
+            }
+         };
+         Thread t = new Thread(r);
+         t.start();
+      }
+   }
+}
